d95f8ce6e6463834421bdbb4a2939e25357c2521
awadhamza <awadhamzaweb@gmail.com>
2020-03-20 20:31:36 -0700

    First Draft push

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import React from 'react';
--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -1,17 +1,44 @@
 import React, { Component } from 'react';
+import backgroundImage from '../../Media/garden6.jpeg';
+import './Landing.css';
+import Button from '@material-ui/core/Button';
+import DoubleArrowIcon from '@material-ui/icons/DoubleArrow';
+import { makeStyles } from '@material-ui/core/styles';
 
-class Landing extends Component {
-    constructor(props){
-        super(props);
-    }
-    
-    render(){
-        //Some js code
-        
-        return(
-            <h1> h </h1>
-        );
-    }
+export default function Landing() {
+
+  const useStyles = makeStyles(theme => ({
+    button: {
+      margin: theme.spacing(1),
+    },
+  }));
+
+  const classes = useStyles();
+
+  return(
+      <div class="main">
+        <div class="top_banner">
+          <img src={backgroundImage} />
+          <div class="banner_text">
+            <div class="big_text">
+              R'Garden?
+            </div>
+            <br/>
+            <div class="small_text">
+              Yes. <b>Yours</b> too.
+            </div>
+
+          </div>
+          <Button
+            variant="contained"
+            color="primary"
+            size="large"
+            className={classes.button}
+            startIcon={<DoubleArrowIcon />}
+          >
+            find out more
+          </Button>
+        </div>
+      </div>
+  );
 }
-
-export default Landing;
