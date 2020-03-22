d580cb58117f272e46bd4acadae05b06be2564b6
awadhamza <awadhamzaweb@gmail.com>
2020-03-21 17:02:05 -0700

    Added hamburger functionality for screen widths smaller than 1000px
    Need to fix hamburger contents still apearing after screen enlargens

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -7,16 +7,45 @@ import {
   Link
 } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars, faCheckSquare, faCoffee , faCaretSquareDown , faCalendarAlt } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faCaretSquareRight, faCheckSquare, faCoffee , faCaretSquareDown , faCalendarAlt } from '@fortawesome/free-solid-svg-icons';
 import './Navbar.css';
 import Events from '../Events/Events';
 
 export default function Navbar() {
 
+  function handleBurger() {
+    //alert('CLICKED!');
+    var div = document.getElementsByClassName("secret_formula")[0];
+
+    if(div.style.display === "block"){
+      // hide
+      div.style.display = "none";
+    } else {
+      // show
+      div.style.display ="block";
+    }
+
+  }
+
+  function handleChoice() {
+    document.getElementsByClassName("secret_formula")[0].style.display = "none";
+  }
+
+  function handleScreenWidthChange() {
+
+    var div = document.getElementsByClassName("secret_formula")[0];
+
+    if(window.screen.width > 1050){
+      div.style.display ="block";
+    } else {
+      div.style.display = "none";
+    }
+  }
+
   return (
 
     <Router>
-
+      {handleScreenWidthChange}
       <div class="nav_bar">
 
         <div class="home_button">
@@ -42,9 +71,43 @@ export default function Navbar() {
 
         </div>
 
-        <span class="halalburger">
-          <FontAwesomeIcon icon={faBars} size="3x" />
+        <span class="halalburger" onClick={handleBurger}>
+          <FontAwesomeIcon id="ham-icon" icon={faBars} size="3x" />
         </span>
+
+        <div class="secret_formula" onClick={handleChoice}>
+
+        <div class="mobile_parent">
+
+          <span class="about-nav hvr-underline-from-left">
+            <FontAwesomeIcon icon={faCaretSquareRight} /> ABOUT
+          </span>
+
+          <div class="dropdown_group">
+            <Link class="hvr-grow-rotate" to="/history">History</Link>
+            <br/>
+            <Link class="hvr-grow-rotate" to="/mission">Mission</Link>
+            <br/>
+            <Link class="hvr-grow-rotate" to="/hoursandlocation">Time & Place</Link>
+            <br/>
+            <Link class="hvr-grow-rotate" to="/media">Media</Link>
+            <br/>
+          </div>
+
+        </div>
+
+          <Link class="ham-item hvr-underline-from-left" to="/events">
+            EVENTS
+          </Link>
+          <br/><br/>
+          <Link class="ham-item hvr-underline-from-left" to="/checkin">
+            CHECK IN
+          </Link>
+          <br/><br/>
+          <Link class="ham-item hvr-underline-from-left" to="/yourplot">
+            YOUR PLOT
+          </Link>
+        </div>
 
         <Link class="nav-item hvr-underline-from-left" to="/events">
           EVENTS
