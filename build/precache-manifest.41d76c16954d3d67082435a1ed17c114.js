d5ae447c451ad2a246e1b1cb4c5208cbf5a04e6c
awadhamza <awadhamzaweb@gmail.com>
2020-03-24 13:29:07 -0700

    New web build

---
+++ build/precache-manifest.41d76c16954d3d67082435a1ed17c114.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30beaac235f297b25e3adb1f90496385",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ea004b64af341a27a42",
+    "url": "/static/css/main.f207c716.chunk.css"
+  },
+  {
+    "revision": "beafa126cf918323febd",
+    "url": "/static/js/2.0be9c659.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/static/js/2.0be9c659.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ea004b64af341a27a42",
+    "url": "/static/js/main.91c6216c.chunk.js"
+  },
+  {
+    "revision": "4664c1b94162ba600101",
+    "url": "/static/js/runtime-main.a954de9a.js"
+  },
+  {
+    "revision": "93750d0d82179c0005b7c96fafab55bb",
+    "url": "/static/media/activities-icon.93750d0d.png"
+  },
+  {
+    "revision": "3ae761c62fbca4c369b41559279bfc62",
+    "url": "/static/media/farm1.3ae761c6.jpeg"
+  },
+  {
+    "revision": "0cf66277a2e6fb5c6fff19d9a663dde8",
+    "url": "/static/media/farm2.0cf66277.jpeg"
+  },
+  {
+    "revision": "f299a7a1bde316fe3c6d7fecf2e7fcad",
+    "url": "/static/media/farm3.f299a7a1.jpeg"
+  },
+  {
+    "revision": "f1a085fcc60ed48d5e494e3de20ba0e1",
+    "url": "/static/media/garden1.f1a085fc.jpeg"
+  },
+  {
+    "revision": "69b6e79bbdba862978a9eedfe7cf8d87",
+    "url": "/static/media/garden2.69b6e79b.jpeg"
+  },
+  {
+    "revision": "4d08d131749505822a0633fe5cdf7849",
+    "url": "/static/media/garden3.4d08d131.jpeg"
+  },
+  {
+    "revision": "c54d07ab8f8de9ad4a21735467f117bf",
+    "url": "/static/media/garden4.c54d07ab.jpg"
+  },
+  {
+    "revision": "9cf92b9ea614547de095dfc00f8177a2",
+    "url": "/static/media/garden6.9cf92b9e.jpeg"
+  },
+  {
+    "revision": "2549921ad65e8dd345b8ceb2644d2974",
+    "url": "/static/media/plant-icon.2549921a.png"
+  },
+  {
+    "revision": "110555437352caaf98672fd2a027499b",
+    "url": "/static/media/team-icon.11055543.png"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.698758bc3f2e24f9f4730feec021968d.js"
+  "/precache-manifest.41d76c16954d3d67082435a1ed17c114.js"
 );
 
 self.addEventListener('message', (event) => {
